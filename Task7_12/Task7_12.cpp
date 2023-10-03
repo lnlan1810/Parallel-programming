9db5eb911e3bf649529fe824773d1877bbc08a36
Le Thi Ngoc Lan <lnlan1810@gmail.com>
2023-10-03 21:52:27 +0300

    task 7_12

---
+++ Task7_12/Task7_12.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+#include <omp.h>
+#include <random>
+#include <chrono>
+
+// Функция для умножения матрицы на вектор
+void matrixVectorMultiply(const std::vector<std::vector<double>>& matrix, const std::vector<double>& vector, std::vector<double>& result) {
+    int numRows = matrix.size();
+    int numCols = matrix[0].size();
+
+#pragma omp parallel for
+    for (int i = 0; i < numRows; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < numCols; j++) {
+            sum += matrix[i][j] * vector[j];
+        }
+        result[i] = sum;
+    }
+}
+
+int main() {
+    // Задаем размеры матрицы и вектора
+    int numRows = 1000;
+    int numCols = 1000;
+    int numThreads = 4; // Количество потоков
+
+    // Инициализируем матрицу, вектор и результат
+    std::vector<std::vector<double>> matrix(numRows, std::vector<double>(numCols));
+    std::vector<double> vector(numCols);
+    std::vector<double> result(numRows);
+
+    // Заполняем матрицу и вектор случайными значениями в диапазоне от 0 до 1000
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> distribution(0.0, 1000.0);
+
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            matrix[i][j] = distribution(gen);
+        }
+    }
+
+    for (int j = 0; j < numCols; j++) {
+        vector[j] = distribution(gen);
+    }
+
+    // Устанавливаем количество потоков
+    omp_set_num_threads(numThreads);
+
+    // Замеряем время выполнения умножения 10 раз и усредняем результаты
+    int numTrials = 10;
+    double totalTime = 0.0;
+
+    for (int trial = 0; trial < numTrials; trial++) {
+        auto start = std::chrono::high_resolution_clock::now();
+        matrixVectorMultiply(matrix, vector, result);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double> duration = end - start;
+        totalTime += duration.count();
+
+        std::cout << "Time taken in trial " << trial + 1 << ": " << duration.count() << " seconds" << std::endl;
+    }
+
+    double averageTime = totalTime / numTrials;
+
+    std::cout << "Average time taken over " << numTrials << " trials: " << averageTime << " seconds" << std::endl;
+
+    return 0;
+}
---
+++ Task_7_12_1/Task7_12_2.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+#include <omp.h>
+
+const int MATRIX_SIZE = 1000;
+const int BLOCK_SIZE = 100;
+
+// Функция для заполнения матрицы случайными числами
+void fillMatrix(std::vector<std::vector<int>>& matrix) {
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        for (int j = 0; j < MATRIX_SIZE; ++j) {
+            matrix[i][j] = rand() % 1001; // случайное число от 0 до 1000
+        }
+    }
+}
+
+// Функция для умножения двух матриц
+void multiplyMatrixBlock(const std::vector<std::vector<int>>& matrixA,
+    const std::vector<std::vector<int>>& matrixB,
+    std::vector<std::vector<int>>& result,
+    int rowStart, int rowEnd, int colStart, int colEnd) {
+    for (int i = rowStart; i < rowEnd; ++i) {
+        for (int j = colStart; j < colEnd; ++j) {
+            result[i][j] = 0;
+            for (int k = 0; k < MATRIX_SIZE; ++k) {
+                result[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    // Инициализация матриц
+    std::vector<std::vector<int>> matrixA(MATRIX_SIZE, std::vector<int>(MATRIX_SIZE));
+    std::vector<std::vector<int>> matrixB(MATRIX_SIZE, std::vector<int>(MATRIX_SIZE));
+    std::vector<std::vector<int>> result(MATRIX_SIZE, std::vector<int>(MATRIX_SIZE));
+
+    fillMatrix(matrixA);
+    fillMatrix(matrixB);
+
+    double total_time = 0.0;
+    const int num_runs = 10;
+
+    for (int run = 0; run < num_runs; ++run) {
+        double start_time = omp_get_wtime();
+
+#pragma omp parallel for
+        for (int i = 0; i < MATRIX_SIZE; i += BLOCK_SIZE) {
+            for (int j = 0; j < MATRIX_SIZE; j += BLOCK_SIZE) {
+                multiplyMatrixBlock(matrixA, matrixB, result, i, i + BLOCK_SIZE, j, j + BLOCK_SIZE);
+            }
+        }
+
+        double end_time = omp_get_wtime();
+        double run_time = end_time - start_time;
+        total_time += run_time;
+        std::cout << "Run " << run + 1 << " time: " << run_time << " seconds" << std::endl;
+    }
+
+    std::cout << "Average time over " << num_runs << " runs: " << total_time / num_runs << " seconds" << std::endl;
+
+    return 0;
+}
+
